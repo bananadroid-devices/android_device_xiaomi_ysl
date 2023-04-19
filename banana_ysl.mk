9d3bed10f33ea48c3406237d245991458724e102
Nasir <130887619+OnFire-Nasir@users.noreply.github.com>
2023-04-19 10:01:16 -0700

    BananaDroid-Ify

--- banana_ysl.mk
+++ banana_ysl.mk
@@ -10,15 +10,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common BananaDroid stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/banana/config/common.mk)
 
-# Inherit some common AOSP flags
-TARGET_BOOT_ANIMATION_RES := 720
-TARGET_SUPPORTS_BLUR := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
+# Inherit some Banana build stuff. 
+ TARGET_BOOT_ANIMATION_RES := 1080 
+ BANANA_MAINTAINER := OnFire-Nasir 
+  
+ # Inherit Gapps stuff 
+ WITH_GAPPS=true 
+ BUILD_CORE_GAPPS := true 
+  
+ # Inherit some AOSP stuffs 
+ TARGET_INCLUDE_STOCK_ARCORE := true 
+ TARGET_INCLUDE_LIVE_WALLPAPERS := true 
+ TARGET_EXCLUDE_MATLOG := true 
+ TARGER_SUPPORTS_NEXT_GEN_ASSISTANT := true 
+ PROCESSOR_MODEL := sm8350 
+ TARGET_FACE_UNLOCK_SUPPORTED := true 
+ TARGET_SUPPORTS_GOOGLE_RECORDER := true 
+ TARGET_SUPPORTS_QUICK_TAP := true 
+ TARGET_USES_BLUR := true
 
 # Inherit from ysl device
 $(call inherit-product, device/xiaomi/ysl/device.mk)
